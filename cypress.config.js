faa0ff3488dce88ab15df1e5fdaac52f4fa6e1d4
aliepratama <aliepratama2@gmail.com>
2024-09-23 13:04:29 +0700

    fix: cypress config for video

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
+  // video: true,
+  // videoCompression: true,
   e2e: {
     baseUrl: "https://automationexercise.com/",
     setupNodeEvents(on, config) {
